c75f1130be30b6a4d06188b14ac028bdd11d3ad1
garaemon <garaemon@gmail.com>
2010-02-22 08:16:16 +0900

    add sample code of gl-widget

---
+++ samples/sample-gl-widget.lisp
@@ -0,0 +1,37 @@
+;;================================================
+;; sample-gl-widget.lisp
+;;
+;; written by R.Ueda (garaemon)
+;;================================================
+
+(require :nurikabe)
+
+
+;;
+;; + <--  +
+;;        |
+;; + -->  +
+;;
+(defmethod nk::glrender ((widget nk::<gl-widget>))
+  (gl:command
+   gl:+polygon+
+   (gl:vertex-3dv (nh:double-vector -0.9 -0.9 0.0))
+   (gl:vertex-3dv (nh:double-vector 0.9 -0.9 0.0))
+   (gl:vertex-3dv (nh:double-vector 0.9 0.9 0.0))
+   (gl:vertex-3dv (nh:double-vector -0.9 0.9 0.0))
+   )
+  )
+
+(defvar *manager* (nk:init-gui :loggingp t :threadingp t))
+(defvar *win* (nk:make-window :width 300 :height 200
+                              :background :white
+                              :name "test window"))
+
+(defvar *widget* (nk::make-gl-widget 'nk::<gl-widget>
+                                     :width 300 :height 200
+                                     :x 0 :y 0
+                                     :background :green
+                                     :parent *win*))
+
+(nk:with-x-serialize (*manager*)
+  (nk:render-widgets *win*))
